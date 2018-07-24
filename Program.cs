aa700ec976167044ce552a62761d8a041d5d8a43
ephemeral9794 <kthm9794.hiro@gmail.com>
2018-07-24 10:10:18 +0900

    表示部分のみ

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marupeke
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] area = {
+				0, 0, 0,
+				0, 0, 0,
+				0, 0, 0
+			};
+			string[,] areastr = {
+				{"〇", "×", "×"},
+				{"〇", "×", "〇"},
+				{"×", "〇", "〇"},
+			};
+
+			// 表示
+			Console.WriteLine($"{areastr[0,0]}｜{areastr[0,1]}｜{areastr[0,2]}");
+			Console.WriteLine("―――――");
+			Console.WriteLine($"{areastr[1,0]}｜{areastr[1,1]}｜{areastr[1,2]}");
+			Console.WriteLine("―――――");
+			Console.WriteLine($"{areastr[2,0]}｜{areastr[2,1]}｜{areastr[2,2]}");
+		}
+	}
+}
